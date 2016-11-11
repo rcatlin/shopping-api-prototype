b14f58fbaea4bcde6a7f5e14920ff2b93c9069a9
Ryan Catlin <ryan.catlin@gmail.com>
2016-11-11 14:35:59 -0500

    Product form controller

--- web/app_dev.php
+++ web/app_dev.php
@@ -23,6 +23,7 @@ $loader = require __DIR__.'/../app/autoload.php';
 Debug::enable();
 
 $kernel = new AppKernel('dev', true);
+$kernel->boot();
 $kernel->loadClassCache();
 $kernel->registerDoctrineTypes();
 
